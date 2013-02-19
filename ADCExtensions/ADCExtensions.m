e11bc439344eefb20e4c07222627ee6db3314e10
Agustin de Cabrera <agustindc@gmail.com>
2013-02-19 00:32:02 -0300

    Initial Commit

---
+++ ADCExtensions/ADCExtensions.m
@@ -0,0 +1,13 @@
+//
+//  ADCExtensions.m
+//  ADCExtensions
+//
+//  Created by Agustín de Cabrera on 19/02/2013.
+//  Copyright (c) 2013 Agustín de Cabrera. All rights reserved.
+//
+
+#import "ADCExtensions.h"
+
+@implementation ADCExtensions
+
+@end
