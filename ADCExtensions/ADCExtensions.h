b749ff8c3540e04811b450819a853427438359fa
Agustin de Cabrera <agustindc@gmail.com>
2013-02-19 09:52:00 -0300

    Added common header.

---
+++ ADCExtensions/ADCExtensions.h
@@ -0,0 +1,13 @@
+//
+//  ADCExtensions.h
+//  ADCExtensions
+//
+//  Created by Agustín de Cabrera on 19/02/2013.
+//  Copyright (c) 2013 Agustín de Cabrera. All rights reserved.
+//
+
+#import "NSArray+ADC.h"
+#import "NSDictionary+ADC.h"
+#import "NSObject+ADC.h"
+#import "NSSet+ADC.h"
+
