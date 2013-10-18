ee41f14118497665ac769d277d0e6703a9e3cc1c
Agustin de Cabrera <agustindc@gmail.com>
2013-10-18 03:32:55 -0300

    Added imports to common header.

--- ADCExtensions/ADCExtensions.h
+++ ADCExtensions/ADCExtensions.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Agustín de Cabrera. All rights reserved.
 //
 
+#import "CGGeometry+ADC.h"
 #import "math+ADC.h"
 
 #import "NSArray+ADC.h"
@@ -24,7 +25,24 @@
 
 #import "NSNumber+ADC.h"
 #import "NSObject+ADC.h"
+#import "NSObject+Casting.h"
 #import "NSString+ADC.h"
+
+#import "UICollectionView+FlowLayout.h"
+#import "UICollectionViewCell+ADC.h"
+
+#import "UIColor+ADC.h"
+#import "UIGestureRecognizer+ADC.h"
+
+#import "UIImage+Cache.h"
+#import "UIImage+Orientation.h"
+#import "UIImage+Tiling.h"
+
+#import "UIImageView+ADC.h"
+#import "UIImageView+Orientation.h"
+
+#import "UITableView+ADC.h"
+#import "UITableViewCell+ADC.h"
 
 #import "UIView+ADC.h"
 
