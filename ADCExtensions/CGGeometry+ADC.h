d17f5cd5b3007e39243b1d0def25c6cb3ae97b4b
Agustin de Cabrera <agustindc@gmail.com>
2015-02-27 14:33:36 -0300

    [CGGeometry] Documented methods.

--- ADCExtensions/CGGeometry+ADC.h
+++ ADCExtensions/CGGeometry+ADC.h
@@ -8,9 +8,12 @@
 
 #import <CoreGraphics/CoreGraphics.h>
 
+// add two points
 CGPoint CGPointAdd(const CGPoint p1, const CGPoint p2);
 
+// return the point within the given rect that is closest to the given point
 CGPoint CGPointClampToRect(const CGPoint point, const CGRect rect);
 
+// return a rect that is within rect2, with the same size as rect1 and closest to it
 CGRect CGRectClampToRect(const CGRect rect1, const CGRect rect2);
 
